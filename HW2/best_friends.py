f3c3a0dfb38351f2350f31ffe45765527b4ad63e
maobedkova <maryobedkova@gmail.com>
2018-06-16 16:45:24 +0200

    final best friends task

--- HW2/best_friends.py
+++ HW2/best_friends.py
@@ -1,8 +1,7 @@
-import nltk
-from collections import Counter
-import numpy as np
-import operator
 import math
+import nltk
+import operator
+from collections import Counter
 
 
 def read_txt(filename):
@@ -37,24 +36,33 @@ def pmi_metric(p_x, p_y, p_xy):
     return math.log(p_xy / (p_x * p_y), 2)
 
 
-def pmi_count(text, distance):
-    """Obtaining dictionaries and retrieveing couts for pmi metric for each bigram"""
-    tokens = read_txt(text)
-    unigr_dict = get_unigrams(tokens)
-    bigr_dict = get_bigrams(tokens, distance)
-    print("Unigram count", len(unigr_dict), "sum", sum(unigr_dict.values()))
-    print("Bigram count", len(bigr_dict), "sum", sum(bigr_dict.values()))
+def pmi_for_text(unigr_dict, bigr_dict):
+    """Retrieveing counts for pmi metric for each bigram"""
+    unigram_sum = sum(unigr_dict.values())
+    bigram_sum = sum(bigr_dict.values())
+
     pmi_dict = {}
     for bigram in bigr_dict:
         unigram_1 = float(unigr_dict[bigram[0]])
         unigram_2 = float(unigr_dict[bigram[1]])
         bigram_value = float(bigr_dict[bigram])
         if unigram_1 >= 10. and unigram_2 >= 10.:   # treshold for disregarding unigrams
-            pmi = pmi_metric(unigram_1 / sum(unigr_dict.values()),
-                            unigram_2 / sum(unigr_dict.values()),
-                            bigram_value / sum(bigr_dict.values()))
+            pmi = pmi_metric(unigram_1 / unigram_sum,
+                            unigram_2 / unigram_sum,
+                            bigram_value / bigram_sum)
             pmi_dict[bigram] = pmi
     return sorted(pmi_dict.items(), key=operator.itemgetter(1), reverse=True)
+
+
+def pmi_count(text, distance):
+    """Obtaining dictionaries and counting pmi scores"""
+    tokens = read_txt(text)
+    unigr_dict = get_unigrams(tokens)
+    bigr_dict = get_bigrams(tokens, distance)
+    print("Unigram count", len(unigr_dict), "sum", sum(unigr_dict.values()))
+    print("Bigram count", len(bigr_dict), "sum", sum(bigr_dict.values()))
+    pmi_dict = pmi_for_text(unigr_dict, bigr_dict)
+    return pmi_dict
 
 
 def out(text, distance):
@@ -72,8 +80,8 @@ if __name__ == "__main__":
     cz_text = "TEXTCZ1.txt"
     en_text = "TEXTEN1.txt"
 
-    # out(cz_text, 1)     # Czech using normal bigrams
-    # out(en_text, 1)     # English using normal bigrams
+    out(cz_text, 1)     # Czech using normal bigrams
+    out(en_text, 1)     # English using normal bigrams
     out(cz_text, 50)    # Czech using bigrams with distance 1-50
-    # out(en_text, 50)    # English using bigrams with distance 1-50
+    out(en_text, 50)    # English using bigrams with distance 1-50
 
